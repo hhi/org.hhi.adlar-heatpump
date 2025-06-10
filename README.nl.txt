346fab014e067e408d13651bc6b18b072330124d
HHi <iphone.hhi@me.com>
2025-06-10 21:02:38 +0200

    capabilities json
    creation of the capabilities json files

---
+++ README.nl.txt
@@ -0,0 +1,1 @@
+Local access to the Aurora series heatpump device.
--- drivers/intelligent-heat-pump/driver.ts
+++ drivers/intelligent-heat-pump/driver.ts
@@ -1,4 +1,7 @@
 import Homey from 'homey';
+import custom_capabilities from './settings/adlar_mapping';
+import capabilities from './settings/adlar_mapping';
+import dps from './settings/adlar_mapping';
 
 module.exports = class MyDriver extends Homey.Driver {
 
@@ -6,26 +9,45 @@ module.exports = class MyDriver extends Homey.Driver {
    * onInit is called when the driver is initialized.
    */
   async onInit() {
+    this.log('MyDriver is initializing...');
+    this.capabilities = capabilities;
+    this.custom_capabilities = custom_capabilities;
+    this.dps = dps;
+
+    // Register capabilities from driver.compose.json
     this.log('MyDriver has been initialized');
   }
 
   /**
-   * onPairListDevices is called when a user is adding a device and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
+   * Returns a list of devices available for pairing.
+   * The user will select one of these devices.
    */
   async onPairListDevices() {
+    // Return devices with a placeholder id; will be replaced after user input
     return [
-      // Example device data, note that `store` is optional
-      // {
-      //   name: 'My Device',
-      //   data: {
-      //     id: 'my-device',
-      //   },
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      // },
+      {
+        name: 'Aurora series',
+        data: { id: 'ZNRB-Device' },
+      }
     ];
+  }
+
+  /**
+   * Handles the data entered by the user after device selection.
+   * Stores device_id and local_key in the session for the selected device.
+   * Updates device.data.id with the entered device_id.
+   */
+  async onPairViewEnterDeviceInfo({ data, session, device }) {
+    // Set the actual device_id as the data.id value
+    if (device && device.data) {
+      device.data.id = data.device_id;
+    }
+    session.setStore({
+      device_id: data.device_id,
+      local_key: data.local_key,
+      selected_device: device, // optional, for reference
+    });
+    return {};
   }
 
 };
